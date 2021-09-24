98fa2d157c32f36e78f6351da564491802d513ac
Den <52295780+josserden@users.noreply.github.com>
2021-09-24 22:37:48 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
 import './sass/main.scss';
+
+import refs from './js/refs';
+import ApiService from './js/apiService';
+import BtnService from './js/btnService';
+import photoCard from './template/photo-card.hbs';
+
+const { formSearch, imageGallery, loadMoreBtn, loadMoreLabel, loadMoreSpinner } = refs;
+
+const imageApi = new ApiService();
+const button = new BtnService({
+  loadMoreBtn: loadMoreBtn,
+  loadMoreLabel: loadMoreLabel,
+  loadMoreSpinner: loadMoreSpinner,
+  classList: 'd-none',
+});
+
+const fetchImg = () => {
+  button.disable();
+
+  imageApi.fetchImg().then(data => {
+    imageGallery.insertAdjacentHTML('beforeend', photoCard(data));
+
+    button.show();
+    button.enable();
+
+    const { height: cardHeight } = imageGallery.firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  });
+};
+
+const searchImg = event => {
+  event.preventDefault();
+  clearGallery();
+  imageApi.resetPage();
+
+  const userRequest = event.currentTarget.elements.query.value;
+  imageApi.query = userRequest;
+  fetchImg();
+
+  formSearch.reset();
+};
+
+formSearch.addEventListener('submit', searchImg);
+loadMoreBtn.addEventListener('click', fetchImg);
+
+function clearGallery() {
+  imageGallery.innerHTML = '';
+}
